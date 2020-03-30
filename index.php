58cb6a0857c340ddc8c2643714dbf7039d561ce3
shaivure <shaivure@gmail.com>
2020-03-30 19:11:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+        <meta name="viewport" content="width=device-width, initial-scale-1" />
+        <meta name="robots" content="follow">
+
+        <title>Page Title</title>
+
+        <base href="/" />
+        <
