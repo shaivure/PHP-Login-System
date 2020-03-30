8c5382656b1b47167258af9f3a04f242229e4ee4
shaivure <shaivure@gmail.com>
2020-03-30 19:45:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<?php echo "Hello World"; ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -17,6 +16,36 @@
     </head>
 
     <body>
+
+        
+        <div class="uk-section uk-container">
+            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+
+                            <form class="uk-form-stacked js-login">
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Email</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Password</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-default" type="submit">Login</button>
+                </div>      
+
+                </form>
+
+            </div>
+        </div>
+
 
         <!-- UIkit JS -->
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/js/uikit.min.js"></script>
