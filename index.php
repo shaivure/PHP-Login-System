13c8beac0046c8f85ae7c512b0bb1182410ec30e
shaivure <shaivure@gmail.com>
2020-03-30 19:16:20 +0100

    html added

--- index.php
+++ index.php
@@ -10,4 +10,17 @@
         <title>Page Title</title>
 
         <base href="/" />
-        <
+        
+        <!-- UIkit CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/css/uikit.min.css" />
+
+    </head>
+
+    <body>
+
+        <!-- UIkit JS -->
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/js/uikit-icons.min.js"></script>
+
+    </body>
+</html>
