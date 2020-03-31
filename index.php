531a7a29887a6f0a1e5ccd3c93b5048037362607
shaivure <shaivure@gmail.com>
2020-03-31 10:56:12 +0100

    Added login and register pages

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+    // Allow the config
+    define('__CONFIG__', true);
+    // Require the config
+    require_once "inc/config.php"; 
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -19,33 +26,18 @@
 
         
         <div class="uk-section uk-container">
-            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+           <?php
+                echo "Hello World. Today is: ";
+                echo date("Y m d");
+            ?>
 
-                            <form class="uk-form-stacked js-login">
-
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-stacked-text">Email</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
-                    </div>
-                </div>
-
-                <div class="uk-margin">
-                    <label class="uk-form-label" for="form-stacked-text">Password</label>
-                    <div class="uk-form-controls">
-                        <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
-                    </div>
-                </div>
-
-                <div class="uk-margin">
-                    <button class="uk-button uk-button-default" type="submit">Login</button>
-                </div>      
-
-                </form>
-
-            </div>
+            <p>
+                <a href="/login/login.php">Login</a>
+                <a href="/login/register.php">Register</a>
+            </p>
         </div>
 
+        <?php require_once "inc/footer.php"; ?>
 
         <!-- UIkit JS -->
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/js/uikit.min.js"></script>
---
+++ register.php
@@ -0,0 +1,65 @@
+<?php 
+    // Allow the config
+    define('__CONFIG__', true);
+    // Require the config
+    require_once "inc/config.php"; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="follow">
+
+        <title>Page Title</title>
+
+        <base href="/" />
+        
+        <!-- UIkit CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/css/uikit.min.css" />
+
+    </head>
+
+    <body>
+
+        
+        <div class="uk-section uk-container">
+            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+
+                <form class="uk-form-stacked js-register">
+
+                <h2>Register</h2>
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Email</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Password</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-default" type="submit">Register</button>
+                </div>      
+
+                </form>
+
+            </div>
+        </div>
+
+        <?php require_once "inc/footer.php"; ?>
+
+        <!-- UIkit JS -->
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.3.7/dist/js/uikit-icons.min.js"></script>
+
+    </body>
+</html>
